c0b547f645f0efe3f468d5cc571d42e51314d36e
RashidArtisan <Harunarrasheeed@gmail.com>
2024-07-30 08:59:34 +0100

    Security Alert System

--- services/auth/login.ts
+++ services/auth/login.ts
@@ -20,7 +20,7 @@ const submitLogin = async (login: string, password: string) => {
         case "CallbackRouteError":
           return { code: 401, message: "Invalid Matric or Password" };
         default:
-          return { code: 500, message: error.message };
+          return { code: 500, message: error.type };
       }
     }
     throw error;
